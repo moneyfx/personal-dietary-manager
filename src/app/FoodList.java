ab00b54baa3866f02ffa670bb6c4271a7954d006
MANI AHMADI ARSHAM <mani.ahmadi.arsham@lightspeedhq.com>
2020-02-25 12:51:00 -0500

    hardcoded data with delete  button

---
+++ src/app/FoodList.java
@@ -0,0 +1,58 @@
+package app;
+
+import javax.swing.*;
+import javax.swing.table.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class FoodList extends JFrame {
+    private JTable table;
+    private DefaultTableModel model;
+    private JButton button;
+
+    static final long serialVersionUID = 100;
+
+    public FoodList() {
+        // headers for the table
+        String[] columns = new String[] { "Name/Retailer", "Time", "Serving/Meal", "Type/Group" };
+
+        // actual data for the table in a 2d array
+        Object[][] data = new Object[][] { { "Banana", "11:00", "1", "bought" }, { "Apple", "12:50", "1", "bought" },
+                { "Mangp", "13:56", "1", "bought" }, { "McDonalds", "16:00", "Lunch", "meat and alternatives" } };
+
+        model = new DefaultTableModel(data, columns);
+        table = new JTable(model);
+        table.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+
+        button = new JButton("Delete row");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                // check for selected row first
+                if (table.getSelectedRow() != -1) {
+                    // remove selected row from the model
+                    model.removeRow(table.getSelectedRow());
+                    // JOptionPane.showMessageDialog(null, "Selected row deleted successfully");
+                }
+            }
+        });
+        add(new JScrollPane(table), BorderLayout.CENTER);
+        add(button, BorderLayout.SOUTH);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        // create table with data
+        // JTable table = new JTable(data, columns);
+
+        // add the table to the frame
+        /*
+         * this.add(new JScrollPane(table));
+         * 
+         * this.setTitle("Consumed food list");
+         * this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); this.pack();
+         * this.setVisible(true);
+         */
+    }
+}
